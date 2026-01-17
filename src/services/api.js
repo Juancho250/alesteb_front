ae5ba2ce1a8c30bf30de698315f39ca551f7ce81
Juancho250 <130582206+Juancho250@users.noreply.github.com>
2026-01-16 21:17:49 -0500

    listo

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://alesteb-back.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
